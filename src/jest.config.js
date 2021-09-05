16c7e8ba888006963847a11b1653f3b08593606a
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-09-05 16:36:14 +0200

    Temba up and running in the starter

---
+++ src/jest.config.js
@@ -0,0 +1,6 @@
+// jest.config.js
+const { defaults } = require("jest-config");
+module.exports = {
+  testMatch: ["**/?(*.)+(test).[jt]s?(x)"],
+  setupFilesAfterEnv: ["jest-extended"],
+};
---
+++ src/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const tembaServer = require("../../temba");
+
+const config = { connectionString: process.env.MONGO_URL };
+const server = tembaServer.create(config);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Temba is running on port ${port}`);
+});
