ef6a4870087d4cc00e10921d9813cf0e1599e98e
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-09-13 22:04:41 +0200

    Preparations for create script

---
+++ bin/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const { execSync } = require("child_process");
+
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: "inherit" });
+  } catch (e) {
+    console.error("Failed to execute ${command}", e);
+    return false;
+  }
+  return true;
+};
+
+const projectName = process.argv[2];
+
+console.log(`Cloning the repository...`);
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/bouwe77/temba-starter ${projectName}`;
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies...`);
+const installDepsCommand = `cd ${projectName} && npm install`;
+const installedDeps = runCommand(installDepsCommand);
+if (!installedDeps) process.exit(-1);
+
+console.log("Done! 🎉");
+console.log("");
+console.log(`cd ${projectName} && npm start`);
