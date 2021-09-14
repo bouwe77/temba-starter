0e90c957b87fd7bd8082d849650181cb48b09ac6
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-09-14 08:49:43 +0200

    Removed config

--- src/server.js
+++ src/server.js
@@ -1,14 +1,6 @@
 import "dotenv/config";
 import { create } from "temba";
 
-// Example config:
-// const config = {
-//   resourceNames: ['movies'],
-//   connectionString: 'mongodb://localhost:27017',
-//   staticFolder: 'build',
-//   apiPrefix: 'api',
-// }
-
 const server = create();
 
 const port = process.env.PORT || 3000;
