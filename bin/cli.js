c14c126777152915bdd01718856004fbe997643f
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-01-19 10:18:34 +0100

    Release 0.4.2: Removed kleur (doesn't add much, KISS)

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import { execSync } from "child_process";
-import kleur from "kleur";
 
 const runCommand = (command) => {
   try {
@@ -14,20 +13,20 @@ const runCommand = (command) => {
 
 const projectName = process.argv[2];
 
-console.log(kleur.green(`Cloning the temba-starter repository...`));
+console.log(`Cloning the temba-starter repository...`);
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/bouwe77/temba-starter ${projectName}`;
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(-1);
 
-console.log(kleur.green(`Installing dependencies...`));
+console.log(`Installing dependencies...`);
 const installDepsCommand = `cd ${projectName} && npm install`;
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) process.exit(-1);
 
-console.log(kleur.green(""));
-console.log(kleur.green("Done! 🎉"));
-console.log(kleur.green(""));
-console.log(kleur.green("Start your Temba server as follows:"));
-console.log(kleur.green(""));
-console.log(kleur.green(`cd ${projectName} && npm start`));
-console.log(kleur.green(""));
+console.log("");
+console.log("Done! 🎉");
+console.log("");
+console.log("Start your Temba server as follows:");
+console.log("");
+console.log(`cd ${projectName} && npm start`);
+console.log("");
